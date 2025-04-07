d108360e985ef7df02037e0bce15ec2f6bbec0e5
BuildTools <unconfigured@null.spigotmc.org>
2025-04-07 01:00:01 -0400

    Added method for class `'formula'`

--- R/ks_rsample.R
+++ R/ks_rsample.R
@@ -1,18 +1,25 @@
 #' Kolmogorov-Smirnov Tests for Two or More Equally-Sized Samples
 #'
 #' @param x a list of numeric vectors, each containing the data values for one
-#'   sample; or a matrix, with each column containing the data values for one sample.
-#'   Each sample must be the same size and must not contain NAs.
+#'   sample; or a matrix, with each column containing the data values for one
+#'   sample. Each sample must be the same size and must not contain NAs.
+#' @param formula a formula, where the left-hand side is a numeric variable in
+#'   `data` and the right-hand side contains a single term whose unique values
+#'   define the groups for the test. The LHS must not contain any missing
+#'   values, and the RHS must define groups of equal size. If the RHS is an
+#'   interaction term, the unique interactions are used to determine groups.
+#'   Parsed using [model.frame()].
+#' @param data an optional data frame (or similar; see [model.frame()])
+#'   containing variables used in `formula`.
 #' @param exact logical; if `TRUE`, or if `NULL` and `(r * D * n) ^ r < 1e4`,
-#'   then an exact p-value is computed; otherwise, an inexact p-value is computed
-#'   according to the setting of `simulate.p.value`. (`r` is the number of samples,
-#'   `D` is the test statistic, and `n` is the size of each sample)
+#'   then an exact p-value is computed; otherwise, an inexact p-value is
+#'   computed according to the setting of `simulate.p.value`. (`r` is the number
+#'   of samples, `D` is the test statistic, and `n` is the size of each sample)
 #' @param simulate.p.value logical; ignored in an exact p-value is computed. If
-#'   `TRUE`, the p-value is estimated using Monte Carlo simulation, and if `FALSE`,
-#'   using the approximation from Böhm and Hornik (2011). `FALSE` is recommended
-#'   except for small sample sizes, as the approximation is fairly good
-#'   at least down to sample
-#'   sizes of about 10.
+#'   `TRUE`, the p-value is estimated using Monte Carlo simulation, and if
+#'   `FALSE`, using the approximation from Böhm and Hornik (2011). `FALSE` is
+#'   recommended except for small sample sizes, as the approximation is fairly
+#'   good at least down to sample sizes of about 10.
 #' @param B integer; number of trials for the Monte Carlo p-value estimation
 #' @param ... further arguments for methods
 #'
@@ -31,28 +38,27 @@ ks.rsample <- function(x, ...) {
 #' @exportS3Method multiks::ks.rsample default
 ks.rsample.default <- function(
   x,
-  exact = NULL,
-  simulate.p.value = FALSE,
-  B = 2e3,
+  exact = NULL, simulate.p.value = FALSE, B = 2e3,
   ...
 ) {
-  data_name <- deparse1(substitute(x))
+  data_name <- (\(data_name = NULL, ...) data_name)(...)
+  if (is.null(data_name)) data_name <- deparse1(substitute(x))
   # input validation
   if (is.list(x)) {
-    if (length(x) < 2) stop(sprintf("'x' has %d elements, but requires at least 2", length(x)))
-    if (!all(vapply(x, is.numeric, logical(1)))) stop("All elements of 'x' must be numeric")
+    if (length(x) < 2) stop(sprintf("'x' has %d elements, but requires at least 2", length(x)), call. = FALSE)
+    if (!all(vapply(x, is.numeric, logical(1)))) stop("All elements of 'x' must be numeric", call. = FALSE)
   } else if (is.matrix(x)) {
-    if (!is.numeric(x)) stop(sprintf("'x' must be numeric, not %d", typeof(x)))
-    if (is.matrix(x) & ncol(x) < 2) stop(sprintf("'x' has %d columns, but requires at least 2", ncol(x)))
+    if (!is.numeric(x)) stop(sprintf("'x' must be numeric, not %d", typeof(x)), call. = FALSE)
+    if (is.matrix(x) & ncol(x) < 2) stop(sprintf("'x' has %d columns, but requires at least 2", ncol(x)), call. = FALSE)
     x <- x |> apply(2, identity, simplify = FALSE)
-  } else stop(sprintf("'x' must be a list or matrix, not a %s", typeof(x)))
-  if (any(is.na(unlist(x)))) stop("'x' contains NA values")
+  } else stop(sprintf("'x' must be a list or matrix, not a %s", typeof(x)), call. = FALSE)
+  if (any(is.na(unlist(x)))) stop("'x' contains NA values", call. = FALSE)
 
   r <- length(x)
   n <- vapply(x, length, integer(1)) |> unique()
 
-  if (length(n) > 1) stop("All samples must be of the same size")
-  if (n < 1) stop("Not enough 'x' values")
+  if (length(n) > 1) stop("All samples must be of the same size", call. = FALSE)
+  if (n < 1) stop("Not enough 'x' values", call. = FALSE)
 
   k <- max_dist(x)
   D <- k / n
@@ -80,6 +86,26 @@ ks.rsample.default <- function(
     ),
     class = c("ks.rsample", "ks.test", "htest")
   )
+}
+
+#' @rdname ks.rsample
+#' @exportS3Method multiks::ks.rsample formula
+ks.rsample.formula <- function(
+  formula, data,
+  exact = NULL, simulate.p.value = FALSE, B = 2e3,
+  ...
+){
+  mframe <- stats::model.frame(formula, data = data)
+  mterms <- attributes(stats::terms(mframe))
+  if (ncol(mterms$factors) != 1) stop(sprintf("'formula' must contain 1 term on RHS, not %d", ncol(mterms$facotrs)), call. = FALSE)
+  if (!is.numeric(mframe[[mterms$response]])) stop(sprintf("'%s' must be numeric, not %s", as.character(mterms$variables[[mterms$response + 1]]), typeof(mframe[[mterms$response]])), call. = FALSE)
+
+  mframe <- mframe[order(mframe[, mterms$factors[, 1] > 0]), ]
+  x <- split(mframe[[mterms$response]], mframe[, mterms$factors[, 1] > 0])
+
+  data_name <- deparse1(formula)
+
+  NextMethod(x = x, data_name = data_name, ...)
 }
 
 max_dist <- function(x) {
